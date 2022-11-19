b2fcb56e12d3ecc3e24485c9707dec1725dc2283
CAMILA ALVES FERREIRA <camila.aferreira24@senacsp.edu.br>
2022-11-18 22:00:04 -0300

    material

--- crud-rest/src/app/app.module.ts
+++ crud-rest/src/app/app.module.ts
@@ -2,21 +2,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { ClienteComponent } from './views/cliente/cliente.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MatButtonModule, MatIconModule, MatSidenavModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClienteComponent
+    ClienteComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
